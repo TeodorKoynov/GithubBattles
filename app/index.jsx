095316e0b9ce07fb1eb9b1e102a8b9d8aa51f544
TeodorKoynov <teodor.koynov2005@gmail.com>
2023-06-04 12:11:21 +0300

    index.jsx refactored to function component

--- app/index.jsx
+++ app/index.jsx
@@ -10,36 +10,62 @@ const Battle = React.lazy(() => import("./components/Battle"));
 const Results = React.lazy(() => import("./components/Results"));
 
 import "./index.css";
+import {useState} from "react";
 
-class App extends React.Component {
-    state = {
-        theme: "light"
-    }
-    toggleTheme = () => {
-        this.setState(({theme}) => ({
-            theme: theme === "light" ? "dark" : "light"
-        }));
+export default function App() {
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => prevTheme === 'light' ? 'dark' : 'light')
     }
 
-    render() {
-        return (
-            <Router>
-                <div className={this.state.theme}>
-                    <div className={"container"}>
-                        <Nav theme={this.state.theme} toggleTheme={this.toggleTheme}/>
-                        <React.Suspense fallback={<Loading/>}>
-                            <Routes>
-                                <Route path={'/'} element={<Popular/>}/>
-                                <Route path={'/battle'} element={<Battle/>}/>
-                                <Route path={'/results'} element={<Results/>}/>
-                            </Routes>
-                        </React.Suspense>
-                    </div>
+    return (
+        <Router>
+            <div className={theme}>
+                <div className={"container"}>
+                    <Nav theme={theme} toggleTheme={toggleTheme}/>
+                    <React.Suspense fallback={<Loading/>}>
+                        <Routes>
+                            <Route path={'/'} element={<Popular/>}/>
+                            <Route path={'/battle'} element={<Battle/>}/>
+                            <Route path={'/results'} element={<Results/>}/>
+                        </Routes>
+                    </React.Suspense>
                 </div>
-            </Router>
-        );
-    }
+            </div>
+        </Router>
+    )
 }
+
+// class App extends React.Component {
+//     state = {
+//         theme: "light"
+//     }
+//     toggleTheme = () => {
+//         this.setState(({theme}) => ({
+//             theme: theme === "light" ? "dark" : "light"
+//         }));
+//     }
+//
+//     render() {
+//         return (
+//             <Router>
+//                 <div className={this.state.theme}>
+//                     <div className={"container"}>
+//                         <Nav theme={this.state.theme} toggleTheme={this.toggleTheme}/>
+//                         <React.Suspense fallback={<Loading/>}>
+//                             <Routes>
+//                                 <Route path={'/'} element={<Popular/>}/>
+//                                 <Route path={'/battle'} element={<Battle/>}/>
+//                                 <Route path={'/results'} element={<Results/>}/>
+//                             </Routes>
+//                         </React.Suspense>
+//                     </div>
+//                 </div>
+//             </Router>
+//         );
+//     }
+// }
 
 const rootElement = document.getElementById('app');
 const root = ReactDOM.createRoot(rootElement);
